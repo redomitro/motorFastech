84560e3dd092027930e9f204805d3b5c86e06240
redomitro <rea.domitrovic@irb.hr>
2026-07-30 12:04:00 +0200

    modified IOC

--- iocs/fastechIOC/iocBoot/iocfastech/st.cmd
+++ iocs/fastechIOC/iocBoot/iocfastech/st.cmd
@@ -16,17 +16,17 @@ dbLoadDatabase "dbd/fastech.dbd"
 fastech_registerRecordDeviceDriver pdbbase
 
 drvAsynIPPortConfigure("L0", "192.168.0.204:3001 UDP", 0, 0, 1)
-EzS2PECreateController("motorExit", "L0", 1, 1000, 1000, 0)
+EzS2PECreateController("motorExit", "L0", 1, 10, 1000, 0)
 
 ## Load record instances
-dbLoadRecords("db/motor.db","SYS=EXIT, SUB=zpos")
+dbLoadRecords("db/motor.db","SYS=EXIT, SUB=zpos, DESC=z-axis control, CONT=motorExit, AXIS=0, MRES=2e-4, TWV=2")
 
 cd "${TOP}/iocBoot/${IOC}"
 
-asynSetTraceIOMask("L0", 0, 2)
-asynSetTraceMask("L0", 0, ERROR|WARNING|FLOW)
-asynSetTraceIOMask("motorExit", 0, 2)
-asynSetTraceMask("motorExit", 0, ERROR|WARNING|FLOW)
+asynSetTraceIOMask("L0", 0, 4)
+asynSetTraceMask("L0", 0, 0x12)
+asynSetTraceIOMask("motorExit", 0, 4)
+asynSetTraceMask("motorExit", 0, 0x12)
 
 iocInit
 
